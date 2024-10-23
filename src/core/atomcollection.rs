af13fdef9d9a62667e3c1b24e5382092645d318f
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-10-23 11:27:31 -0400

    update bonds

--- src/core/atomcollection.rs
+++ src/core/atomcollection.rs
@@ -5,7 +5,9 @@ pub struct AtomCollection {
     coords: Vec<[f32; 3]>,
     resvs: Vec<i32>,
     chains: Vec<String>,
-    // atom_type: Vec<String>, // Adding atom type as a fixed field
+    bonds: Option<Vec<Bond>>,
+    // atom_type: Vec<String>,
+    //
     // // ... other fixed fields
     // dynamic_fields: HashMap<String, Vec<Box<dyn Any>>>,
 }
@@ -79,6 +81,30 @@ impl From<&PSEData> for AtomCollection {
             .flat_map(|c| c.get_coords_as_vec())
             .collect();
 
+        // Pymol: most of the descriptive data is there
+        let pymol_bonds: Vec<&pseutils::pymolparsing::parsing::Bond> =
+            mols.iter().flat_map(|mol| mol.bond.iter()).collect();
+
+        let bonds = pymol_bonds
+            .iter()
+            .map(|b| Bond {
+                atom1: b.index_1,
+                atom2: b.index_2,
+                order: match b.order {
+                    0 => BondOrder::Unset,
+                    1 => BondOrder::Single,
+                    2 => BondOrder::Double,
+                    3 => BondOrder::Triple,
+                    4 => BondOrder::Quadruple,
+                    _ => {
+                        println!("Bond Order not found: {:?}", b.order);
+                        panic!()
+                    }
+                },
+            })
+            .collect();
+
+        // specific fields
         let resns: Vec<String> = atoms.iter().map(|a| a.resn.to_string()).collect();
         let resvs: Vec<i32> = atoms.iter().map(|a| a.resv).collect();
         let chains: Vec<String> = atoms.iter().map(|a| a.chain.to_string()).collect();
@@ -88,9 +114,37 @@ impl From<&PSEData> for AtomCollection {
             coords,
             resvs,
             chains,
+            bonds: Some(bonds),
         };
         ac
     }
+}
+
+pub struct Bond {
+    atom1: i32,
+    atom2: i32,
+    order: BondOrder,
+    // id
+    // stereo
+    // unique_id
+    // has_setting
+}
+
+#[repr(u8)]
+/// BondOrder:
+/// https://www.biotite-python.org/latest/apidoc/biotite.structure.BondType.html#biotite.structure.BondType
+/// see also: http://cdk.github.io/cdk/latest/docs/api/org/openscience/cdk/Bond.html
+pub enum BondOrder {
+    /// Used if the actual type is unknown
+    Unset,
+    /// Single bond
+    Single,
+    /// Double bond
+    Double,
+    /// Triple bond
+    Triple,
+    /// A quadruple bond
+    Quadruple,
 }
 
 // // A view struct to represent a subset of atoms
@@ -124,7 +178,6 @@ mod tests {
         assert!(psedata.version == 3000000);
         let names = psedata.get_session_names();
         assert_eq!(names.len(), 2);
-
         let ac = AtomCollection::from(&psedata);
         assert_eq!(ac.size, 1519);
         assert_eq!(ac.coords.len(), 1519);
