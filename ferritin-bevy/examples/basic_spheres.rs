b4df6ea9cbd53153a973dd519c6ae8fc6edecccc
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-10-27 12:20:19 -0400

    remove unused code

--- ferritin-bevy/examples/basic_spheres.rs
+++ ferritin-bevy/examples/basic_spheres.rs
@@ -41,13 +41,6 @@ fn main() {
             }),
         ))
         .add_systems(Startup, setup)
-        // .add_systems(
-        //     Update,
-        //     (
-        //         update_protein_meshes,
-        //         focus_camera_on_proteins,
-        //     ),
-        // )
         .run();
 }
 
@@ -129,59 +122,3 @@ fn setup(mut commands: Commands) {
         ..default()
     });
 }
-
-// fn update_protein_meshes(
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<StandardMaterial>>,
-//     query: Query<(Entity, &Structure), (With<Structure>, Without<Handle<Mesh>>)>,
-// ) {
-//     println!("I'm in the update_protein_mesh function!");
-//     println!("Total entities with Structure: {}", query.iter().count());
-//     for (entity, protein) in query.iter() {
-//         println!("Working on {:?}", entity);
-//         let mesh = protein.to_mesh();
-//         let mesh_handle = meshes.add(mesh);
-//         let material = materials.add(StandardMaterial {
-//             base_color: Color::srgb(0.8, 0.7, 0.6),
-//             metallic: 0.1,
-//             perceptual_roughness: 0.5,
-//             ..default()
-//         });
-
-//         commands.entity(entity).insert(PbrBundle {
-//             mesh: mesh_handle,
-//             material,
-//             ..default()
-//         });
-//     }
-// }
-
-// fn focus_camera_on_proteins(
-//     mut camera_query: Query<&mut Transform, (With<Camera>, Without<Structure>)>,
-//     protein_query: Query<&Transform, With<Structure>>,
-// ) {
-//     if let Ok(mut camera_transform) = camera_query.get_single_mut() {
-//         if let Some(center) = calculate_center_of_proteins(&protein_query) {
-//             let camera_position = center + Vec3::new(0.0, 50.0, 100.0);
-//             camera_transform.translation = camera_position;
-//             camera_transform.look_at(center, Vec3::Y);
-//         }
-//     }
-// }
-
-// fn calculate_center_of_proteins(
-//     protein_query: &Query<&Transform, With<Structure>>,
-// ) -> Option<Vec3> {
-//     let mut total = Vec3::ZERO;
-//     let mut count = 0;
-//     for transform in protein_query.iter() {
-//         total += transform.translation;
-//         count += 1;
-//     }
-//     if count > 0 {
-//         Some(total / count as f32)
-//     } else {
-//         None
-//     }
-// }
