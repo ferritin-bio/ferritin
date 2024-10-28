4ea7c158dc7d2f027ca3dac619f77bea531a459f
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-10-28 16:12:19 -0400

    update conversion nexting

--- ferritin-core/src/core/mod.rs
+++ ferritin-core/src/core/mod.rs
@@ -1,5 +1,6 @@
 mod atomcollection;
 mod constants;
+mod conversions;
 mod featureizer;
 
 pub use atomcollection::{AtomCollection, Bond, BondOrder};
--- ferritin-core/src/lib.rs
+++ ferritin-core/src/lib.rs
@@ -1,4 +1,3 @@
-mod conversions;
 pub mod core;
 pub use core::{AtomCollection, Bond, BondOrder};
 pub use pdbtbx::Element;
