8e2df1faeb3d6644940f96756aecfb8b43e05304
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-10-29 09:22:38 -0400

    move Bonds to its own file

--- ferritin-core/src/core/atomcollection.rs
+++ ferritin-core/src/core/atomcollection.rs
@@ -1,5 +1,5 @@
+use super::bonds::{Bond, BondOrder};
 use super::constants::get_bonds_canonical20;
-use super::{Bond, BondOrder};
 use crate::core::selection::{AtomSelector, AtomView, Selection};
 use itertools::{izip, Itertools};
 use pdbtbx::Element;
@@ -259,40 +259,6 @@ impl AtomCollection {
                 ),
         );
         starts
-    }
-}
-
-#[repr(u8)]
-#[derive(Debug, PartialEq)]
-/// BondOrder:
-/// https://www.biotite-python.org/latest/apidoc/biotite.structure.BondType.html#biotite.structure.BondType
-/// see also: http://cdk.github.io/cdk/latest/docs/api/org/openscience/cdk/Bond.html
-pub enum BondOrder {
-    /// Used if the actual type is unknown
-    Unset,
-    /// Single bond
-    Single,
-    /// Double bond
-    Double,
-    /// Triple bond
-    Triple,
-    /// A quadruple bond
-    Quadruple,
-}
-
-impl BondOrder {
-    pub fn match_bond(bond_int: i32) -> BondOrder {
-        match bond_int {
-            0 => BondOrder::Unset,
-            1 => BondOrder::Single,
-            2 => BondOrder::Double,
-            3 => BondOrder::Triple,
-            4 | 5 | 6 => BondOrder::Quadruple,
-            _ => {
-                println!("Bond Order not found: {}", bond_int);
-                panic!()
-            }
-        }
     }
 }
 
