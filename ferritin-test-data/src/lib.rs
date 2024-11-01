057efd85ef086b600822b3180ea60dfd3404c105
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-01 14:10:34 -0400

    update ferritin test data

--- ferritin-test-data/src/lib.rs
+++ ferritin-test-data/src/lib.rs
@@ -1,1 +1,33 @@
-pub const PROTEIN_EXAMPLE_01: &[u8] = include_bytes!("../data/structures/101m.cif");
+use std::fs;
+use tempfile::{Builder, NamedTempFile};
+
+#[derive(Debug)]
+pub struct TestFile {
+    filebinary: &'static [u8],
+    suffix: &'static str,
+}
+
+impl TestFile {
+    pub fn protein_01() -> Self {
+        Self {
+            filebinary: include_bytes!("../data/structures/101m.cif"),
+            suffix: "cif",
+        }
+    }
+
+    pub fn pymol_01() -> Self {
+        Self {
+            filebinary: include_bytes!("../data/pymol/example.pse"),
+            suffix: "pse",
+        }
+    }
+
+    pub fn to_tempfile(&self) -> std::io::Result<NamedTempFile> {
+        let temp = Builder::new()
+            .suffix(&format!(".{}", self.suffix))
+            .tempfile()?;
+
+        fs::write(&temp, self.filebinary)?;
+        Ok(temp)
+    }
+}
