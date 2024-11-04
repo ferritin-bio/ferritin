69d99f56fb2e5aa652edb3c5d36dd5ca1619fafe
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-03 21:54:56 -0500

    update

--- ferritin-featurizers/tests/test_cli.rs
+++ ferritin-featurizers/tests/test_cli.rs
@@ -8,15 +8,13 @@ fn test_cli_command() {
     let (ciffile, _tmp) = TestFile::protein_01().create_temp().unwrap();
     let tempfile = tempfile::NamedTempFile::new().unwrap();
 
-    let tempfile = "test.safetensor";
     let mut cmd = Command::cargo_bin("ferritin-featurizers").unwrap();
 
     cmd.arg("featurize")
         .arg("--input")
         .arg(ciffile)
         .arg("--output")
-        // .arg(tempfile.path())
-        .arg(tempfile);
+        .arg(tempfile.path());
 
     cmd.assert().success();
 }
