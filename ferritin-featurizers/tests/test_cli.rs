e18a6be46fd58c833eb84faedc87d7bdd13a8cac
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-03 20:54:36 -0500

    get a cli test running

--- ferritin-featurizers/tests/test_cli.rs
+++ ferritin-featurizers/tests/test_cli.rs
@@ -1,13 +1,20 @@
 use assert_cmd::Command;
+use ferritin_test_data::TestFile;
+use tempfile;
 
 #[test]
 fn test_cli_command() {
+    // input and output tempfiles
+    let (ciffile, _tmp) = TestFile::protein_01().create_temp().unwrap();
+    let tempfile = tempfile::NamedTempFile::new().unwrap();
+
     let mut cmd = Command::cargo_bin("ferritin-featurizers").unwrap();
+
     cmd.arg("featurize")
         .arg("--input")
-        .arg("test")
+        .arg(ciffile)
         .arg("--output")
-        .arg("test");
+        .arg(tempfile.path());
 
     cmd.assert().success();
 }
