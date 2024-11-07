6155788a420225c1f367a5ac9432bf95515de3cd
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-07 16:35:44 -0500

    update add

--- ferritin-featurizers/src/models/amplify/amplify.rs
+++ ferritin-featurizers/src/models/amplify/amplify.rs
@@ -87,15 +87,13 @@ impl AMPLIFYConfig {
     }
 }
 
-// EncoderBlock implementation
-//
+/// Amplify EncoderBlock implementation
+///
 // example 01: T5: https://github.com/huggingface/candle/blob/e2b6b367fa852ed30ac532f8d77cd8479c7ed092/candle-transformers/src/models/t5.rs#L331
 //
 // Example 01: FFN: https://github.com/huggingface/candle/blob/e2b6b367fa852ed30ac532f8d77cd8479c7ed092/candle-transformers/src/models/distilbert.rs#L198
 // ffn: FeedForward,
-//
 // // Clean Implementation
-//
 // Example: https://github.com/huggingface/candle/blob/e2b6b367fa852ed30ac532f8d77cd8479c7ed092/candle-transformers/src/models/glm4.rs#L340
 // SwiGLu Implementation:  https://github.com/facebookresearch/xformers/blob/main/xformers/ops/swiglu_op.py#L462
 //
@@ -104,77 +102,45 @@ pub struct EncoderBlock {
     k: Linear,
     v: Linear,
     wo: Linear,
-    // resid_dropout: Dropout,
     w12: Linear,
     w3: Linear,
     attention_norm: RmsNorm, // <----- Check These
     ffn_norm: RmsNorm,
+    // resid_dropout: Dropout,
     // ffn_dropout: Dropout,
 }
 
 impl EncoderBlock {
     pub fn new(config: &AMPLIFYConfig, vb: VarBuilder) -> Result<Self> {
-        // let d_head = config.hidden_size / config.num_attention_heads;
-        // // Attention layers
-        // let q = Linear::new(
-        //     vb.pp("q").get_with_hints(
-        //         (config.hidden_size, config.hidden_size),
-        //         "weight",
-        //         candle_nn::init::ZERO,
-        //     )?,
-        //     if config.att_bias {
-        //         Some(vb.pp("q").get_with_hints(
-        //             config.hidden_size,
-        //             "bias",
-        //             candle_nn::init::ZERO,
-        //         )?)
-        //     } else {
-        //         None
-        //     },
-        // );
-        // // Similar initialization for k, v, and wo...
-        // // FFN initialization based on activation type
-        // let multiple_of = 8;
-        // let intermediate_size =-
-        //     (2 * config.intermediate_size / 3).div_ceil(multiple_of) * multiple_of;
-        // let ffn = FeedForward::load(config, vb);
-        // FFN::SwiGLU(SwiGLUFFN::new(
-        //     config.hidden_size,
-        //     intermediate_size,
-        //     config.hidden_size,
-        //     config.ffn_bias,
-        //     vb.pp("ffn"),
-        // )?)
+        let d_head = config.hidden_size / config.num_attention_heads;
 
-        // let attention_norm: Box<dyn Module> = if config.rms_norm {
-        //     Box::new(RMSNorm::new(
-        //         config.hidden_size,
-        //         config.norm_eps,
-        //         vb.pp("attention_norm"),
-        //     )?)
-        // } else {
-        //     Box::new(LayerNorm::new(
-        //         config.hidden_size,
-        //         config.norm_eps,
-        //         vb.pp("attention_norm"),
-        //     )?)
-        // };
+        let multiple_of = 8;
+        let multiple_of = 8;
+        let intermediate_size = (cfg.intermediate_size * 2) / 3;
+        let intermediate_size = multiple_of * ((intermediate_size + multiple_of - 1) / multiple_of);
+        let q = linear(config.hidden_size, config.hidden_size, vb.pp("q"))?;
+        let k = linear(config.hidden_size, config.hidden_size, vb.pp("k"))?;
+        let v = linear(config.hidden_size, config.hidden_size, vb.pp("v"))?;
+        let wo = linear(config.hidden_size, config.hidden_size, vb.pp("wo"))?;
+        let w12 = linear_no_bias(intermediate_size * 2, config.hidden_size, vb.pp("ffn.w12"))?;
+        let w3 = linear_no_bias(config.hidden_size, intermediate_size, vb.pp("ffn.w3"))?;
+        let [ffn_norm, attention_norm] = [
+            rms_norm(config.hidden_size, config.norm_eps, vb.pp("ffn_norm"))?,
+            rms_norm(config.hidden_size, config.norm_eps, vb.pp("attention_norm"))?,
+        ];
 
-        // // Similar for ffn_norm...
-
-        // Ok(Self {
-        //     config: config.clone(),
-        //     q,
-        //     k,
-        //     v,
-        //     wo,
-        //     resid_dropout: Dropout::new(config.dropout_prob),
-        //     ffn,
-        //     attention_norm,
-        //     ffn_norm,
-        //     ffn_dropout: Dropout::new(config.dropout_prob),
-        // })
-        unimplemented!();
+        Ok(Self {
+            q,
+            k,
+            v,
+            wo,
+            w12,
+            w3,
+            attention_norm,
+            ffn_norm,
+            // ffn_dropout: Dropout::new(config.dropout_prob),
+            // resid_dropout: Dropout::new(config.dropout_prob),
+        })
     }
 
     fn forward(
@@ -274,7 +240,67 @@ impl AMPLIFY {
         output_hidden_states: bool,
         output_attentions: bool,
     ) -> Result<ModelOutput> {
-        unimplemented!()
+        let mut hidden_states = vec![];
+        let mut attentions = vec![];
+
+        // Process attention mask if provided
+        let attention_mask = if let Some(mask) = pad_mask {
+            if !mask.all_close(&mask.zeros_like()?, 1e-6, 1e-6)? {
+                Some(mask.unsqueeze(1)?.unsqueeze(1)?.expand((
+                    mask.dim(0)?,
+                    self.config.num_attention_heads,
+                    mask.dim(-1)?,
+                    mask.dim(-1)?,
+                ))?)
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        // Get appropriate length of freqs_cis
+        let freqs_cis = self.freqs_cis.narrow(0, 0, src.dim(1)?)?;
+
+        // Embedding layer
+        let mut x = self.encoder.forward(src)?;
+
+        // Transform through encoder blocks
+        for layer in self.transformer_encoder.iter() {
+            let (new_x, attn) =
+                layer.forward(&x, attention_mask.as_ref(), &freqs_cis, output_attentions)?;
+            x = new_x;
+
+            if output_hidden_states {
+                hidden_states.push(x.clone()?);
+            }
+            if output_attentions {
+                if let Some(attn) = attn {
+                    attentions.push(attn);
+                }
+            }
+        }
+
+        // Final layer norm and decoder
+        let logits = if self.config.layer_norm_before_last_layer {
+            self.decoder.forward(&self.layer_norm_2.forward(&x)?)?
+        } else {
+            self.decoder.forward(&x)?
+        };
+
+        Ok(ModelOutput {
+            logits,
+            hidden_states: if output_hidden_states {
+                Some(hidden_states)
+            } else {
+                None
+            },
+            attentions: if output_attentions {
+                Some(attentions)
+            } else {
+                None
+            },
+        })
     }
 
     pub fn load(vb: VarBuilder, cfg: &AMPLIFYConfig) -> Result<Self> {
