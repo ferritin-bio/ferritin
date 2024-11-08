cfb5a5d89abe825176d0c3f0245cba5b6094f17d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-08 11:04:10 -0500

    update readme and main

--- ferritin-featurizers/examples/amplify/main.rs
+++ ferritin-featurizers/examples/amplify/main.rs
@@ -18,16 +18,14 @@ fn main() -> Result<()> {
         revision.to_string(),
     ));
 
-    // Download the safetensors file
-    let weights_path = repo.get("model.safetensors")?;
-
     // Load and analyze the safetensors file
+    let weights_path = repo.get("model.safetensors")?;
     let weights = std::fs::read(&weights_path)?;
     let tensors = SafeTensors::deserialize(&weights)?;
 
     // Print all tensor names and their metadata
     println!("Model tensors:");
-    for tensor_name in tensors.names() {
+    tensors.names().iter().for_each(|tensor_name| {
         if let Ok(tensor_info) = tensors.tensor(tensor_name) {
             println!(
                 "Tensor: {:<44}  ||  Shape: {:?}",
@@ -35,51 +33,17 @@ fn main() -> Result<()> {
                 tensor_info.shape(),
             );
         }
-    }
+    });
 
     // https://github.com/huggingface/candle/blob/main/candle-examples/examples/clip/main.rs#L91C1-L92C101
     let vb = unsafe {
         VarBuilder::from_mmaped_safetensors(&[weights_path.clone()], DType::F32, &Device::Cpu)?
     };
-
-    // Pull a specific Tensor out of the variable builder.....
-    let _Tensor1 = vb.get(&[3424, 640], "transformer_encoder.0.ffn.w12.weight")?;
-    println!("Example Tensor Shape: {:?}", _Tensor1.shape());
-
-    // Load the Weights into the Model
     let config = AMPLIFYConfig::default().amp_120m();
     let model = AMPLIFY::load(vb, &config)?;
-
-    // try loading the tokenizer
     let tokenizer = repo.get("tokenizer.json")?;
     let protein_tokenizer = ProteinTokenizer::new(tokenizer)?;
-
-    let tokens = vec![
-        "M".to_string(),
-        "E".to_string(),
-        "T".to_string(),
-        "V".to_string(),
-        "A".to_string(),
-        "L".to_string(),
-        "<pad>".to_string(),
-        "<unk>".to_string(),
-        "<mask>".to_string(),
-        "<bos>".to_string(),
-        "<eos>".to_string(),
-    ];
-
-    let encoded = protein_tokenizer.encode(&tokens, Some(10), true, true)?;
-    let decoded = protein_tokenizer.decode(
-        &encoded
-            .to_vec1::<i64>()?
-            .iter()
-            .map(|&x| x as u32)
-            .collect::<Vec<u32>>(),
-        true,
-    )?;
-
-    println!("Encoded: {:?}", encoded);
-    println!("Decoded: {}", decoded);
+    let pmatrix = protein_tokenizer.encode(&["METVAL".to_string()], Some(20), true, false);
 
     Ok(())
 }
