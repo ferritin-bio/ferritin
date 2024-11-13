76d4ff5fec2a122270e74b437af6ac75bf58fa83
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-13 12:40:07 -0500

    clean

--- ferritin-featurizers/tests/test_amplify_model.rs
+++ ferritin-featurizers/tests/test_amplify_model.rs
@@ -23,23 +23,18 @@ fn test_amplify_round_trip() -> Result<(), Box<dyn std::error::Error>> {
     // Load and analyze the safetensors file
     let weights_path = repo.get("model.safetensors")?;
     let weights = std::fs::read(&weights_path)?;
-    let tensors = SafeTensors::deserialize(&weights)?;
-
     let vb = unsafe {
         VarBuilder::from_mmaped_safetensors(&[weights_path.clone()], DType::F32, &Device::Cpu)?
     };
+
     let config = AMPLIFYConfig::default().amp_120m();
     let model = AMPLIFY::load(vb, &config)?;
-
-    println!("Successfully created the model!");
     let tokenizer = repo.get("tokenizer.json")?;
     let protein_tokenizer = ProteinTokenizer::new(tokenizer)?;
 
     let sprot_01 = "MAFSAEDVLKEYDRRRRMEALLLSLYYPNDRKLLDYKEWSPPRVQVECPKAPVEWNNPPSEKGLIVGHFSGIKYKGEKAQASEVDVNKMCCWVSKFKDAMRRYQGIQTCKIPGKVLSDLDAKIKAYNLTVEGVEGFVRYSRVTKQHVAAFLKELRHSKQYENVNLIHYILTDKRVDIQHLEKDLVKDFKALVESAHRMRQGHMINVKYILYQLLKKHGHGPDGPDILTVKTGSKGVLYDDSFRKIYTDLGWKFTPL";
     let pmatrix = protein_tokenizer.encode(&[sprot_01.to_string()], None, false, false)?;
     let pmatrix = pmatrix.unsqueeze(0)?; // [batch, length] <- add batch of 1 in this case
-
-    println!("Commence Encoding:");
     let encoded = model.forward(&pmatrix, None, false, false)?;
 
     // Choosing ARGMAX. We expect this to be the most predicted sequence.
