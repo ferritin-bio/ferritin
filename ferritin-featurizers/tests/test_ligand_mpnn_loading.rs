d2ab30d3f7026e6401bcb28e43e70dc30191ccb2
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-24 13:06:57 -0500

    loading the model in a test now works

--- ferritin-featurizers/tests/test_ligand_mpnn_loading.rs
+++ ferritin-featurizers/tests/test_ligand_mpnn_loading.rs
@@ -3,11 +3,10 @@ mod tests {
     use candle_core::pickle::read_pth_tensor_info;
     use candle_core::{DType, Device, Error};
     use candle_nn::VarBuilder;
-    use ferritin_test_data::TestFile;
     use ferritin_featurizers::{ProteinMPNN, ProteinMPNNConfig};
+    use ferritin_test_data::TestFile;
 
     #[test]
-
     fn test_load_ligandmpnn() -> Result<(), Error> {
         let (mpnn_file, _handle) = TestFile::ligmpnn_pmpnn_01().create_temp()?;
         let device = Device::Cpu;
@@ -18,7 +17,6 @@ mod tests {
         // * `file` - The path to the .pth file.
         // * `verbose` - Whether to print debug information.
         // * `key` - Optional key to retrieve `state_dict` from the pth file.
-
         let mut tensors = read_pth_tensor_info(mpnn_file, false, Some("model_state_dict"))?;
         tensors.sort_by(|a, b| a.name.cmp(&b.name));
         for tensor_info in tensors.iter() {
@@ -31,10 +29,17 @@ mod tests {
 
             println!("    {:?}", tensor_info);
         }
-
-        let vb = VarBuilder();
-        let config =
-        let pmpnn_model = ProteinMPNN::load(vb, config);
+        Ok(())
+    }
+    #[test]
+    fn test_load_ligandmpnn2() -> Result<(), Error> {
+        let (mpnn_file, _handle) = TestFile::ligmpnn_pmpnn_01().create_temp()?;
+        let vb = candle_nn::VarBuilder::from_pth(mpnn_file, DType::F32, &Device::Cpu)?;
+        println!("{:?}", &vb.device());
+        println!("{:?}", &vb.dtype);
+        let pconf = ProteinMPNNConfig::proteinmpnn();
+        println!("{:?}", &pconf);
+        let pmpnn = ProteinMPNN::load(vb, &pconf);
         Ok(())
     }
 }
