e107c173dadf05cb85a8a9110f52c3fa2783ab1e
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-11-26 20:03:08 -0500

    fix syntax

--- ferritin-core/src/bonds.rs
+++ ferritin-core/src/bonds.rs
@@ -57,7 +57,7 @@ impl BondOrder {
             3 => BondOrder::Triple,
             4 | 5 | 6 => BondOrder::Quadruple,
             _ => {
-                panic!(format!("Bond Order not found: {}", bond_int))
+                panic!("Bond Order not found: {}", bond_int);
             }
         }
     }
