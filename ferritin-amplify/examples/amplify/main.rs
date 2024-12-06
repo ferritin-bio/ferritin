509bc04c4da0b27567cbf6f9618a0d04acf37ee9
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-06 10:46:05 -0500

    simplify the AMPLIFY Example

--- ferritin-amplify/examples/amplify/main.rs
+++ ferritin-amplify/examples/amplify/main.rs
@@ -7,34 +7,6 @@ use ferritin_amplify::{AMPLIFYConfig, ProteinTokenizer, AMPLIFY};
 use safetensors::SafeTensors;
 
 fn main() -> Result<()> {
-    let model_id = "chandar-lab/AMPLIFY_120M";
-    let revision = "main";
-    let api = Api::new()?;
-    let repo = api.repo(Repo::with_revision(
-        model_id.to_string(),
-        RepoType::Model,
-        revision.to_string(),
-    ));
-    let weights_path = repo.get("model.safetensors")?;
-
-    // Available for printing Tensor data....
-    let print_tensor_info = false;
-    if print_tensor_info {
-        println!("Model tensors:");
-        let weights = std::fs::read(&weights_path)?;
-        let tensors = SafeTensors::deserialize(&weights)?;
-        tensors.names().iter().for_each(|tensor_name| {
-            if let Ok(tensor_info) = tensors.tensor(tensor_name) {
-                println!(
-                    "Tensor: {:<44}  ||  Shape: {:?}",
-                    tensor_name,
-                    tensor_info.shape(),
-                );
-            }
-        });
-    }
-
-    // https://github.com/huggingface/candle/blob/main/candle-examples/examples/clip/main.rs#L91C1-L92C101
     println!("Loading the Amplify Model ......");
     #[cfg(target_os = "macos")]
     let use_gpu = false;
@@ -42,29 +14,21 @@ fn main() -> Result<()> {
     let use_gpu = true;
 
     let dev = device(use_gpu)?;
-    let vb =
-        unsafe { VarBuilder::from_mmaped_safetensors(&[weights_path.clone()], DType::F32, &dev)? };
-    let config = AMPLIFYConfig::amp_120m();
-    let model = AMPLIFY::load(vb, &config)?;
-
-    println!("Tokenizing and Modelling a Sequence from Swissprot...");
-    let tokenizer = repo.get("tokenizer.json")?;
-    let protein_tokenizer = ProteinTokenizer::new(tokenizer)?;
+    let (tokenizer, amplify) = AMPLIFY::load_from_huggingface(dev.clone())?;
     let sprot_01 = "MAFSAEDVLKEYDRRRRMEALLLSLYYPNDRKLLDYKEWSPPRVQVECPKAPVEWNNPPSEKGLIVGHFSGIKYKGEKAQASEVDVNKMCCWVSKFKDAMRRYQGIQTCKIPGKVLSDLDAKIKAYNLTVEGVEGFVRYSRVTKQHVAAFLKELRHSKQYENVNLIHYILTDKRVDIQHLEKDLVKDFKALVESAHRMRQGHMINVKYILYQLLKKHGHGPDGPDILTVKTGSKGVLYDDSFRKIYTDLGWKFTPL";
-    let pmatrix = protein_tokenizer.encode(&[sprot_01.to_string()], None, false, false)?;
+    let pmatrix = tokenizer.encode(&[sprot_01.to_string()], None, false, false)?;
     println!("Pmatrix Device: {:?}", pmatrix.device());
     let pmatrix = pmatrix.to_device(&dev)?;
     println!("Pmatrix Device: {:?}", pmatrix.device());
     let pmatrix = pmatrix.unsqueeze(0)?; // [batch, length] <- add batch of 1 in this case
-    let encoded = model.forward(&pmatrix, None, false, false)?;
-
+    let encoded = amplify.forward(&pmatrix, None, false, false)?;
     println!("Assessing the Predictions.......");
     // As of Nov 13 this is definitely not right....
     // Input:   MAFSAEDVLKEYDRRRRMEALLLSLYYPNDRKLLDYKEWSPPRVQVECPKAPVEWNNPPSEKGLIVGHFSGIKYKGEKAQASEVDVNKMCCWVSKFKDAMRRYQGIQTCKIPGKVLSDLDAKIKAYNLTVEGVEGFVRYSRVTKQHVAAFLKELRHSKQYENVNLIHYILTDKRVDIQHLEKDLVKDFKALVESAHRMRQGHMINVKYILYQLLKKHGHGPDGPDILTVKTGSKGVLYDDSFRKIYTDLGWKFTPL
     // Output:  MSVQLNIVGQSAAWTHGAAVCATCAQTFWPMSRGRQPPVNMSRFTARCTECIWYEAAFNARFNFVHLYNCGPNMSECLANMSWWYACQFGVHMSKSHYCGNKPLGTDNTKMMHHRECTSTVVWKHWPLCKVTVCYRHGLVSCTMHQRSTWTPRNEASWVPEWETSTPEHTCGDYWACQMPAGHGVCCCMMTEHWKPHTRVVCQTIEMWTYLQTYYYFWGVPEPCHHHIWTEPMPTSTSTSYDVVMYTTSGFGQHHW
     let predictions = encoded.logits.argmax(D::Minus1)?;
     let indices: Vec<u32> = predictions.to_vec2()?[0].to_vec();
-    let decoded = protein_tokenizer.decode(indices.as_slice(), true)?;
+    let decoded = tokenizer.decode(indices.as_slice(), true)?;
     println!("Encoded Logits Dimension: {:?}, ", encoded.logits);
     println!("indices: {:?}", indices);
     println!("Decoded Values: {}", decoded.replace(" ", ""));
