f537f2de3d8ff56c2ef3946cd17f1af1223eb905
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-06 14:48:44 -0500

    update deps

--- ferritin-wasm-examples/amplify/build-lib.sh
+++ ferritin-wasm-examples/amplify/build-lib.sh
@@ -1,2 +1,5 @@
-cargo build --target wasm32-unknown-unknown --release
+# cargo build --target wasm32-unknown-unknown --release
+
+cargo +nightly build --target wasm32-unknown-unknown --release
+
 wasm-bindgen ../../target/wasm32-unknown-unknown/release/m.wasm --out-dir build --target web
