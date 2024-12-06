b76e4831d70e991a6b31c4d98a26029fed11c30c
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-06 11:36:33 -0500

    simplify the call

--- ferritin-amplify/examples/amplify/main.rs
+++ ferritin-amplify/examples/amplify/main.rs
@@ -1,27 +1,25 @@
 use anyhow::Result;
-use candle_core::{DType, Device, D};
+use candle_core::D;
 use candle_examples::device;
-use candle_hf_hub::{api::sync::Api, Repo, RepoType};
-use candle_nn::VarBuilder;
-use ferritin_amplify::{AMPLIFYConfig, ProteinTokenizer, AMPLIFY};
-use safetensors::SafeTensors;
+use ferritin_amplify::AMPLIFY;
 
 fn main() -> Result<()> {
-    println!("Loading the Amplify Model ......");
+    println!("Determining the Device ......");
     #[cfg(target_os = "macos")]
     let use_gpu = false;
     #[cfg(not(target_os = "macos"))]
     let use_gpu = true;
-
     let dev = device(use_gpu)?;
+
+    println!("Loading the Amplify Model ......");
     let (tokenizer, amplify) = AMPLIFY::load_from_huggingface(dev.clone())?;
     let sprot_01 = "MAFSAEDVLKEYDRRRRMEALLLSLYYPNDRKLLDYKEWSPPRVQVECPKAPVEWNNPPSEKGLIVGHFSGIKYKGEKAQASEVDVNKMCCWVSKFKDAMRRYQGIQTCKIPGKVLSDLDAKIKAYNLTVEGVEGFVRYSRVTKQHVAAFLKELRHSKQYENVNLIHYILTDKRVDIQHLEKDLVKDFKALVESAHRMRQGHMINVKYILYQLLKKHGHGPDGPDILTVKTGSKGVLYDDSFRKIYTDLGWKFTPL";
     let pmatrix = tokenizer.encode(&[sprot_01.to_string()], None, false, false)?;
-    println!("Pmatrix Device: {:?}", pmatrix.device());
     let pmatrix = pmatrix.to_device(&dev)?;
-    println!("Pmatrix Device: {:?}", pmatrix.device());
+
     let pmatrix = pmatrix.unsqueeze(0)?; // [batch, length] <- add batch of 1 in this case
     let encoded = amplify.forward(&pmatrix, None, false, false)?;
+
     println!("Assessing the Predictions.......");
     // As of Nov 13 this is definitely not right....
     // Input:   MAFSAEDVLKEYDRRRRMEALLLSLYYPNDRKLLDYKEWSPPRVQVECPKAPVEWNNPPSEKGLIVGHFSGIKYKGEKAQASEVDVNKMCCWVSKFKDAMRRYQGIQTCKIPGKVLSDLDAKIKAYNLTVEGVEGFVRYSRVTKQHVAAFLKELRHSKQYENVNLIHYILTDKRVDIQHLEKDLVKDFKALVESAHRMRQGHMINVKYILYQLLKKHGHGPDGPDILTVKTGSKGVLYDDSFRKIYTDLGWKFTPL
