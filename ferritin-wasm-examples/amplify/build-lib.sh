e4b6f569370bc64aa59a6e068e632418e4d40a46
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-06 15:12:28 -0500

    resolve WASM compilation issues

--- ferritin-wasm-examples/amplify/build-lib.sh
+++ ferritin-wasm-examples/amplify/build-lib.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+set -e  # Exit on error
+
+cargo clean
+mkdir -p target/wasm32-unknown-unknown/release
+
+cargo update
+
+
+cargo +nightly build \
+    --target wasm32-unknown-unknown \
+    --release \
+    -Z build-std=std,panic_abort \
+    --no-default-features
+
+
+
 # cargo build --target wasm32-unknown-unknown --release
-
-cargo +nightly build --target wasm32-unknown-unknown --release
-
-wasm-bindgen ../../target/wasm32-unknown-unknown/release/m.wasm --out-dir build --target web
+# rustup +nightly target add wasm32-unknown-unknown
+# cargo +nightly build --target wasm32-unknown-unknown --release
+#
+# wasm-bindgen ../../target/wasm32-unknown-unknown/release/m.wasm --out-dir build --target web
