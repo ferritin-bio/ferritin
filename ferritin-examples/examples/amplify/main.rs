2067cd16b341580f8a1c5d083c7c3521056f290d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-07 17:34:58 -0500

    ferritin-examples/examples/amplify/main.rs

--- ferritin-examples/examples/amplify/main.rs
+++ ferritin-examples/examples/amplify/main.rs
@@ -15,32 +15,6 @@ struct Args {
     /// Run on CPU rather than on GPU.
     #[arg(long)]
     cpu: bool,
-    // /// The model to use, check out available models: https://huggingface.co/models?library=sentence-transformers&sort=trending
-    // #[arg(long)]
-    // model_id: Option<String>,
-
-    // #[arg(long)]
-    // revision: Option<String>,
-
-    // /// When set, compute embeddings for this prompt.
-    // #[arg(long)]
-    // prompt: Option<String>,
-
-    // /// Use the pytorch weights rather than the safetensors ones
-    // #[arg(long)]
-    // use_pth: bool,
-
-    // /// The number of times to run the prompt.
-    // #[arg(long, default_value = "1")]
-    // n: usize,
-
-    // /// L2 normalization for embeddings.
-    // #[arg(long, default_value = "true")]
-    // normalize_embeddings: bool,
-
-    // /// Use tanh based approximation for Gelu instead of erf implementation.
-    // #[arg(long, default_value = "false")]
-    // approximate_gelu: bool,
 }
 
 impl Args {
