f2256f13cf912c5e1c0ec8d1c1e67d076865e6d2
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-07 16:56:48 -0500

    amplify docs

--- ferritin-amplify/src/lib.rs
+++ ferritin-amplify/src/lib.rs
@@ -1,7 +1,17 @@
 //! ferritin-amplify
+//!
+//!
 //! - [amplify model](https://github.com/chandar-lab/AMPLIFY)
 //! - [amplify hf - 120M](https://huggingface.co/chandar-lab/AMPLIFY_120M)
 //!
-
+//! To try and example of this model:
+//!
+//! ```shell
+//! cargo run --example amplify
+//! cargo run --example amplify --features metal
+//! ```
+//!
+//!
+//!
 mod amplify;
 pub use amplify::amplify::{AMPLIFYConfig, ModelOutput, AMPLIFY};
