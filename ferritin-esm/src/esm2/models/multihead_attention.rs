2b98d018c66daf5f0c94584090f89f64c5b8ad53
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-13 18:12:06 -0500

    mh attention

--- ferritin-esm/src/esm2/models/multihead_attention.rs
+++ ferritin-esm/src/esm2/models/multihead_attention.rs
@@ -5,7 +5,7 @@
 
 use super::esm2::ESM2Config;
 use super::rotary_embedding::RotaryEmbedding;
-use candle_core::{Device, Module, Result, Tensor};
+use candle_core::{Device, Module, Result, Tensor, D},;
 use candle_nn::{self as nn, linear, ops, VarBuilder};
 
 // pub fn utils_softmax(x: &Tensor, dim: i64, onnx_trace: bool) -> Result<Tensor> {
@@ -64,7 +64,6 @@ pub struct MultiheadAttention {
     // kdim: i64,
     // vdim: i64,
     // qkv_same_dim: bool,
-    // dropout: f64,
     // head_dim: i64,
     // scaling: f64,
     // self_attention: bool,
@@ -74,12 +73,13 @@ pub struct MultiheadAttention {
     // add_zero_attn: bool,
     // onnx_trace: bool,
     // enable_torch_version: bool,
+    // dropout: f64,
+    // incremental_state: FairseqIncrementalState,
     q_proj: nn::Linear,
     k_proj: nn::Linear,
     v_proj: nn::Linear,
     out_proj: nn::Linear,
     rot_emb: Option<RotaryEmbedding>,
-    // incremental_state: FairseqIncrementalState,
 }
 
 impl MultiheadAttention {
@@ -128,277 +128,61 @@ impl MultiheadAttention {
         })
     }
 
-    // pub fn forward(
-    //     &self,
-    //     query: &Tensor,
-    //     key: Option<&Tensor>,
-    //     value: Option<&Tensor>,
-    //     key_padding_mask: Option<&Tensor>,
-    //     incremental_state: Option<&mut HashMap<String, HashMap<String, Option<Tensor>>>>,
-    //     need_weights: bool,
-    //     static_kv: bool,
-    //     attn_mask: Option<&Tensor>,
-    //     before_softmax: bool,
-    //     need_head_weights: bool,
-    // ) -> Result<(Tensor, Option<Tensor>)> {
-    //     let need_weights = need_weights || need_head_weights;
-    //     let (tgt_len, bsz, embed_dim) = query.dims3()?;
-    //     // assert_eq!(embed_dim, self.embed_dim as usize);
-    //     // if !self.rot_emb.is_some()
-    //     //     && self.enable_torch_version
-    //     //     && !self.onnx_trace
-    //     //     && incremental_state.is_none()
-    //     //     && !static_kv
-    //     //     && !need_head_weights
-    //     // {
-    //     //     return multihead_attention_forward(
-    //     //         query,
-    //     //         key.unwrap(),
-    //     //         value.unwrap(),
-    //     //         self.embed_dim,
-    //     //         self.num_heads,
-    //     //         &self.q_proj,
-    //     //         &self.k_proj,
-    //     //         &self.v_proj,
-    //     //         self.bias_k.as_ref(),
-    //     //         self.bias_v.as_ref(),
-    //     //         self.add_zero_attn,
-    //     //         self.dropout,
-    //     //         &self.out_proj,
-    //     //         self.training,
-    //     //         key_padding_mask,
-    //     //         need_weights,
-    //     //         attn_mask,
-    //     //     );
-    //     // }
+    pub fn forward(
+        &self,
+        query: &Tensor,
+        key: Option<&Tensor>,
+        value: Option<&Tensor>,
+        key_padding_mask: Option<&Tensor>,
+        incremental_state: Option<&mut HashMap<String, HashMap<String, Option<Tensor>>>>,
+        need_weights: bool,
+        static_kv: bool,
+        attn_mask: Option<&Tensor>,
+        before_softmax: bool,
+        need_head_weights: bool,
+    ) -> Result<(Tensor, Option<Tensor>)> {
+        // Todo: heck these
+        let num_heads = 10;
+        let head_dim = 10;
 
-    //     // let mut saved_state = None;
-    //     // if let Some(inc_state) = incremental_state {
-    //     //     saved_state = self.get_incremental_state(Some(inc_state), "attn_state");
-    //     //     if let Some(saved_state_ref) = &saved_state {
-    //     //         if saved_state_ref.contains_key("prev_key") && static_kv {
-    //     //             assert!(self.encoder_decoder_attention && !self.self_attention);
-    //     //         }
-    //     //     }
-    //     // }
+        let need_weights = need_weights || need_head_weights;
+        let (tgt_len, bsz, embed_dim) = query.dims3()?;
+        let q = self.q_proj.forward(query)?;
+        let k = self.k_proj.forward(query)?;
+        let v = self.v_proj.forward(query)?;
+        // let q = (q * config.scaling)?;
+        let q = q
+            .reshape((tgt_len, bsz * num_heads as usize, head_dim as usize))?
+            .transpose(0, 1)?;
+        k = k
+            .reshape((D::minus1, bsz * num_heads as usize, head_dim as usize))?
+            .transpose(0, 1)?;
+        v = v.reshape((-1, bsz * num_heads as usize, head_dim as usize))?
+            .transpose(0, 1)?;
+        let src_len = k.dim(1)?;
+        let (q, k) = rot_emb.forward(&q, &k)?;
+        let attn_weights = q.matmul(&k.transpose(1, 2)?)?;
+        let attn_weights = attn_weights.softmax(2)?;
+        // let attn_weights = ops::dropout(&attn_weights, self.dropout, self.training)?;
+        let attn = attn_weights.matmul(&v)?;
+        let attn = attn.transpose(0, 1)?
+                .reshape((tgt_len, bsz, self.embed_dim as usize))?;
+        let attn = self.out_proj.forward(&attn)?;
+        let attn_weights = if need_weights {
+            let attn_weights = attn_weights
+                .reshape((bsz, self.num_heads as usize, tgt_len, src_len))?
+                .transpose(0, 1)?;
+            if !need_head_weights {
+                Some(attn_weights.mean(0)?)
+            } else {
+                Some(attn_weights)
+            }
+        } else {
+            None
+        };
 
-    //     // let q = if self.self_attention {
-    //     //     self.q_proj.forward(query)?
-    //     // } else if self.encoder_decoder_attention {
-    //     //     self.q_proj.forward(query)?
-    //     // } else {
-    //     //     assert!(key.is_some() && value.is_some());
-    //     //     self.q_proj.forward(query)?
-    //     // };
-
-    //     // let k = if self.self_attention {
-    //     //     self.k_proj.forward(query)?
-    //     // } else if self.encoder_decoder_attention && key.is_none() {
-    //     //     assert!(value.is_none());
-    //     //     Tensor::zeros((), query.device())?
-    //     // } else {
-    //     //     assert!(key.is_some());
-    //     //     self.k_proj.forward(key.unwrap())?
-    //     // };
-
-    //     // let v = if self.self_attention {
-    //     //     self.v_proj.forward(query)?
-    //     // } else if self.encoder_decoder_attention && value.is_none() {
-    //     //     Tensor::zeros((), query.device())?
-    //     // } else {
-    //     //     assert!(value.is_some());
-    //     //     self.v_proj.forward(value.unwrap())?
-    //     // };
-
-    //     // let q = (q * self.scaling)?;
-
-    //     // let (mut k, mut v) = if self.bias_k.is_some() {
-    //     //     assert!(self.bias_v.is_some());
-    //     //     let bias_k = self.bias_k.as_ref().unwrap();
-    //     //     let bias_v = self.bias_v.as_ref().unwrap();
-    //     //     let bias_k = bias_k.broadcast_as((1, 1, bias_k.dim(2)?))?;
-    //     //     let bias_v = bias_v.broadcast_as((1, 1, bias_v.dim(2)?))?;
-    //     //     let k = Tensor::cat(&[&k, &bias_k], 1)?;
-    //     //     let v = Tensor::cat(&[&v, &bias_v], 1)?;
-    //     //     if let Some(attn_mask) = attn_mask {
-    //     //         let attn_mask = Tensor::cat(
-    //     //             &[
-    //     //                 attn_mask,
-    //     //                 Tensor::zeros((attn_mask.dim(0)?, 1), query.device())?,
-    //     //             ],
-    //     //             1,
-    //     //         )?;
-    //     //     }
-    //     //     if let Some(key_padding_mask) = key_padding_mask {
-    //     //         let key_padding_mask = Tensor::cat(
-    //     //             &[
-    //     //                 key_padding_mask,
-    //     //                 Tensor::zeros((key_padding_mask.dim(0)?, 1), query.device())?,
-    //     //             ],
-    //     //             1,
-    //     //         )?;
-    //     //     }
-    //     //     (k, v)
-    //     // } else {
-    //     //     (k, v)
-    //     // };
-    //     // let q = q
-    //     //     .reshape((
-    //     //         tgt_len,
-    //     //         bsz * self.num_heads as usize,
-    //     //         self.head_dim as usize,
-    //     //     ))?
-    //     //     .transpose(0, 1)?;
-    //     // if k.dim()? > 0 {
-    //     //     k = k
-    //     //         .reshape((-1, bsz * self.num_heads as usize, self.head_dim as usize))?
-    //     //         .transpose(0, 1)?;
-    //     // }
-    //     // if v.dim()? > 0 {
-    //     //     v = v
-    //     //         .reshape((-1, bsz * self.num_heads as usize, self.head_dim as usize))?
-    //     //         .transpose(0, 1)?;
-    //     // }
-    //     // if let Some(saved_state_ref) = &mut saved_state {
-    //     //     if saved_state_ref.contains_key("prev_key") {
-    //     //         let prev_key = saved_state_ref.get("prev_key").unwrap().as_ref().unwrap();
-    //     //         let prev_key = prev_key.reshape((
-    //     //             bsz * self.num_heads as usize,
-    //     //             -1,
-    //     //             self.head_dim as usize,
-    //     //         ))?;
-    //     //         if static_kv {
-    //     //             k = prev_key;
-    //     //         } else {
-    //     //             k = Tensor::cat(&[prev_key, &k], 1)?;
-    //     //         }
-    //     //     }
-    //     //     if saved_state_ref.contains_key("prev_value") {
-    //     //         let prev_value = saved_state_ref.get("prev_value").unwrap().as_ref().unwrap();
-    //     //         let prev_value = prev_value.reshape((
-    //     //             bsz * self.num_heads as usize,
-    //     //             -1,
-    //     //             self.head_dim as usize,
-    //     //         ))?;
-    //     //         if static_kv {
-    //     //             v = prev_value;
-    //     //         } else {
-    //     //             v = Tensor::cat(&[prev_value, &v], 1)?;
-    //     //         }
-    //     //     }
-    //     //     saved_state_ref.insert("prev_key".to_string(), Some(k.clone()));
-    //     //     saved_state_ref.insert("prev_value".to_string(), Some(v.clone()));
-    //     //     if let Some(inc_state) = incremental_state {
-    //     //         self.set_incremental_state(Some(inc_state), "attn_state", saved_state_ref.clone());
-    //     //     }
-    //     // }
-    //     // let src_len = k.dim(1)?;
-    //     // if let Some(key_padding_mask) = key_padding_mask {
-    //     //     assert_eq!(key_padding_mask.dim(0)? as i64, bsz);
-    //     //     assert_eq!(key_padding_mask.dim(1)? as i64, src_len);
-    //     // }
-    //     // if self.add_zero_attn {
-    //     //     src_len += 1;
-    //     //     k = Tensor::cat(
-    //     //         &[
-    //     //             &k,
-    //     //             Tensor::zeros((k.dim(0)?, 1, k.dim(2)?), query.device())?,
-    //     //         ],
-    //     //         1,
-    //     //     )?;
-    //     //     v = Tensor::cat(
-    //     //         &[
-    //     //             &v,
-    //     //             Tensor::zeros((v.dim(0)?, 1, v.dim(2)?), query.device())?,
-    //     //         ],
-    //     //         1,
-    //     //     )?;
-    //     //     if let Some(attn_mask) = attn_mask {
-    //     //         let attn_mask = Tensor::cat(
-    //     //             &[
-    //     //                 attn_mask,
-    //     //                 Tensor::zeros((attn_mask.dim(0)?, 1), query.device())?,
-    //     //             ],
-    //     //             1,
-    //     //         )?;
-    //     //     }
-    //     //     if let Some(key_padding_mask) = key_padding_mask {
-    //     //         let key_padding_mask = Tensor::cat(
-    //     //             &[
-    //     //                 key_padding_mask,
-    //     //                 Tensor::zeros((key_padding_mask.dim(0)?, 1), query.device())?,
-    //     //             ],
-    //     //             1,
-    //     //         )?;
-    //     //     }
-    //     // }
-    //     // if let Some(rot_emb) = &self.rot_emb {
-    //     //     let (q, k) = rot_emb.forward(&q, &k)?;
-    //     // }
-    //     // let attn_weights = q.matmul(&k.transpose(1, 2)?)?;
-    //     // assert_eq!(
-    //     //     attn_weights.dims(),
-    //     //     &[bsz * self.num_heads as usize, tgt_len, src_len]
-    //     // );
-    //     // if let Some(attn_mask) = attn_mask {
-    //     //     let attn_mask = attn_mask.unsqueeze(0)?;
-    //     //     if self.onnx_trace {
-    //     //         let attn_mask = attn_mask.repeat((attn_weights.dim(0)?, 1, 1))?;
-    //     //     }
-    //     //     attn_weights = (&attn_weights + attn_mask)?;
-    //     // }
-    //     // if let Some(key_padding_mask) = key_padding_mask {
-    //     //     let attn_weights =
-    //     //         attn_weights.reshape((bsz, self.num_heads as usize, tgt_len, src_len))?;
-
-    //     //     let key_padding_mask = key_padding_mask
-    //     //         .unsqueeze(1)?
-    //     //         .unsqueeze(2)?
-    //     //         .to_dtype(candle_core::DType::Bool)?;
-    //     //     let attn_weights = attn_weights
-    //     //         .masked_fill(&key_padding_mask, f32::NEG_INFINITY)?
-    //     //         .reshape((bsz * self.num_heads as usize, tgt_len, src_len))?;
-    //     // }
-    //     // if before_softmax {
-    //     //     return Ok((attn_weights, Some(v)));
-    //     // }
-    //     // let attn_weights = if self.onnx_trace {
-    //     //     attn_weights.to_dtype(candle_core::DType::F32)?.softmax(2)?
-    //     // } else {
-    //     //     attn_weights.softmax(2)?
-    //     // };
-    //     // let attn_weights = ops::dropout(&attn_weights, self.dropout, self.training)?;
-    //     // let attn = attn_weights.matmul(&v)?;
-    //     // assert_eq!(
-    //     //     attn.dims(),
-    //     //     &[
-    //     //         bsz * self.num_heads as usize,
-    //     //         tgt_len,
-    //     //         self.head_dim as usize
-    //     //     ]
-    //     // );
-    //     // let attn = if self.onnx_trace && attn.dim(1)? == 1 {
-    //     //     attn.reshape((tgt_len, bsz, self.embed_dim as usize))?
-    //     // } else {
-    //     //     attn.transpose(0, 1)?
-    //     //         .reshape((tgt_len, bsz, self.embed_dim as usize))?
-    //     // };
-    //     // let attn = self.out_proj.forward(&attn)?;
-    //     // let attn_weights = if need_weights {
-    //     //     let attn_weights = attn_weights
-    //     //         .reshape((bsz, self.num_heads as usize, tgt_len, src_len))?
-    //     //         .transpose(0, 1)?;
-    //     //     if !need_head_weights {
-    //     //         Some(attn_weights.mean(0)?)
-    //     //     } else {
-    //     //         Some(attn_weights)
-    //     //     }
-    //     // } else {
-    //     //     None
-    //     // };
-
-    //     Ok((attn, attn_weights))
-    // }
+        Ok((attn, attn_weights))
+    }
 
     // fn _append_prev_key_padding_mask(
     //     key_padding_mask: Option<&Tensor>,
