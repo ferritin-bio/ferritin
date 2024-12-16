3114e4f80cbe75ca2e8f990b17d6d967b3341c0d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-16 14:09:27 -0500

    update gitignore

--- ferritin-plms/ferritin-esm/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! ferritin-esm
-//!
-
-pub use ferritin_plms::::models::esmc::{ESMCConfig, ESMC};
-
-pub use ferritin_plms::::models::esm2::{ESM2Config, ESM2};
--- ferritin-plms/ferritin-ligandmpnn/Readme.md
+++
@@ -1,4 +0,0 @@
-# ferritin-ligandmpnn
-
-- utilities to convert sequence formats (PDB; mmcif) to ML-ready tensors.
-- CLI to handle the above.
