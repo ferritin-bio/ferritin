389e2cb4d34cd4755ecd8ee0a097e02b65ed51af
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-20 18:52:22 -0500

    src and Cargo

--- ferritin-core/src/featurize/mod.rs
+++ ferritin-core/src/featurize/mod.rs
@@ -8,87 +8,76 @@
 //! - Geometric features like distances, angles
 //! - Chemical features like hydrophobicity, charge
 //! - Evolutionary features from MSA profiles
-
-#[cfg(feature = "candle-backend")]
-pub mod candle_impl;
-
-#[cfg(feature = "ndarray-backend")]
-pub mod ndarray_impl;
-
-#[cfg(feature = "candle-backend")]
-pub use candle_impl::*;
-
-#[cfg(feature = "ndarray-backend")]
-pub use ndarray_impl::*;
-
 use crate::AtomCollection;
-
+use ndarray::{Array1, Array2, Array3};
 use std::collections::HashMap;
 
-pub trait StructureFeatures<T> {
+pub trait StructureFeatures {
     type Error;
 
     /// Convert amino acid sequence to numeric representation
-    fn encode_amino_acids(&self, device: Option<&Device>) -> Result<T, Self::Error>;
+    fn encode_amino_acids(&self) -> Result<Array2<f32>, Self::Error>;
 
     /// Convert structure into complete feature set
-    fn featurize(&self, device: Option<&Device>) -> Result<ProteinFeatures<T>, Self::Error>;
+    fn featurize(&self) -> Result<ProteinFeatures, Self::Error>;
 
     /// Get residue indices
-    fn get_res_index(&self, device: Option<&Device>) -> Vec<u32>;
+    fn get_res_index(&self) -> Array1<u32>;
 
     /// Extract backbone atom coordinates (N, CA, C, O)
-    fn to_numeric_backbone_atoms(&self, device: Option<&Device>) -> Result<T, Self::Error>;
+    fn to_numeric_backbone_atoms(&self) -> Result<Array3<f32>, Self::Error>;
 
     /// Extract all atom coordinates in standard ordering
-    fn to_numeric_atom37(&self, device: Option<&Device>) -> Result<T, Self::Error>;
+    fn to_numeric_atom37(&self) -> Result<Array3<f32>, Self::Error>;
 
     /// Extract ligand atom coordinates and properties
-    fn to_numeric_ligand_atoms(&self, device: Option<&Device>) -> Result<(T, T, T), Self::Error>;
+    fn to_numeric_ligand_atoms(
+        &self,
+    ) -> Result<(Array2<f32>, Array2<f32>, Array2<f32>), Self::Error>;
 
     /// Convert to PDB format
     fn to_pdb(&self);
 }
 
-pub struct ProteinFeatures<T> {
+pub struct ProteinFeatures {
     /// Protein amino acid sequence encoding
-    pub sequence: T,
+    pub sequence: Array2<f32>,
     /// Protein atom coordinates
-    pub coordinates: T,
+    pub coordinates: Array3<f32>,
     /// Protein atom mask
-    pub atom_mask: Option<T>,
+    pub atom_mask: Option<Array2<bool>>,
     /// Ligand coordinates
-    pub ligand_coords: T,
+    pub ligand_coords: Array2<f32>,
     /// Ligand atom types
-    pub ligand_types: T,
+    pub ligand_types: Array2<f32>,
     /// Ligand mask
-    pub ligand_mask: Option<T>,
+    pub ligand_mask: Option<Array2<bool>>,
     /// Residue indices
-    pub residue_indices: T,
+    pub residue_indices: Array1<u32>,
     /// Chain labels
     pub chain_labels: Option<Vec<f64>>,
     /// Chain letters
     pub chain_letters: Vec<String>,
     /// Chain mask
-    pub chain_mask: Option<T>,
+    pub chain_mask: Option<Array2<bool>>,
     /// List of chains
     pub chain_list: Vec<String>,
 }
 
-impl<T> ProteinFeatures<T> {
-    pub fn get_coords(&self) -> &T {
+impl ProteinFeatures {
+    pub fn get_coords(&self) -> &Array3<f32> {
         &self.coordinates
     }
 
-    pub fn get_sequence(&self) -> &T {
+    pub fn get_sequence(&self) -> &Array2<f32> {
         &self.sequence
     }
 
-    pub fn get_sequence_mask(&self) -> Option<&T> {
+    pub fn get_sequence_mask(&self) -> Option<&Array2<bool>> {
         self.atom_mask.as_ref()
     }
 
-    pub fn get_residue_index(&self) -> &T {
+    pub fn get_residue_index(&self) -> &Array1<u32> {
         &self.residue_indices
     }
 
@@ -97,10 +86,7 @@ impl<T> ProteinFeatures<T> {
     ) -> Result<
         (Vec<String>, HashMap<String, usize>, HashMap<usize, String>),
         Box<dyn std::error::Error>,
-    >
-    where
-        T: AsRef<[u32]>,
-    {
+    > {
         // Implementation
         todo!()
     }
