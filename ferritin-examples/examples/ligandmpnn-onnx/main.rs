e6f2b12d08c6779ac9915e9d87697adfd7dedcef
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2024-12-29 16:22:23 -0500

    simplify

--- ferritin-examples/examples/ligandmpnn-onnx/main.rs
+++ ferritin-examples/examples/ligandmpnn-onnx/main.rs
@@ -1,24 +1,13 @@
 use anyhow::Result;
-use candle_core::Device;
-use ferritin_core::{AtomCollection, StructureFeatures};
-use ferritin_onnx_models::{
-    ndarray_to_tensor_f32, tensor_to_ndarray_f32, tensor_to_ndarray_i64, LigandMPNN,
-    LigandMPNNModels,
-};
+use ferritin_core::AtomCollection;
+use ferritin_onnx_models::LigandMPNN;
 use ferritin_test_data::TestFile;
-
-use ort::{
-    execution_providers::CUDAExecutionProvider,
-    session::{builder::GraphOptimizationLevel, Session},
-    value::Tensor,
-};
 
 fn main() -> Result<()> {
     println!("Loading the Model and Tokenizer.......");
     let (protfile, _handle) = TestFile::protein_01().create_temp()?;
     let (pdb, _) = pdbtbx::open(protfile).expect("PDB/CIF");
     let ac = AtomCollection::from(&pdb);
-
     let model = LigandMPNN::new().unwrap();
     let logits = model.run_model(ac, 10, 0.1).unwrap();
     println!("{:?}", logits);
