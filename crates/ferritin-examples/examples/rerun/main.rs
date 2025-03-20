7c8eaf35b4e95f1db6b5a6019cc48388d017f71a
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-03-20 13:44:16 -0400

    meshes display but the colors and brightness do noe

--- crates/ferritin-examples/examples/rerun/main.rs
+++ crates/ferritin-examples/examples/rerun/main.rs
@@ -1,17 +1,19 @@
 use anyhow::Result;
 use bevy::prelude::*;
-use ferritin_bevy::{Structure, ToRerun};
+use ferritin_bevy::{
+    ColorScheme, RenderOptions, Structure, StructurePlugin, StructureSettings, ToRerun,
+};
 use ferritin_core::load_structure;
 use ferritin_test_data::TestFile;
 use rerun::demo_util::color_spiral;
-use rerun::{self as rr};
+use rerun::{self as rr, Mesh3D};
 use std::f32::consts::TAU;
 
 fn main() -> Result<()> {
     let (molfile, _handle) = TestFile::protein_01().create_temp()?;
 
-    let _chalky = StandardMaterial {
-        base_color: Color::srgb(0.9, 0.9, 0.9), // Light gray color
+    let chalky = StandardMaterial {
+        base_color: Color::srgb(0.4, 0.4, 0.4), // Light gray color
         perceptual_roughness: 1.0,              // Maximum roughness for a matte look
         metallic: 0.0,                          // No metallic properties
         reflectance: 0.1,                       // Low reflectance
@@ -24,37 +26,38 @@ fn main() -> Result<()> {
     };
 
     let ac = load_structure(molfile).unwrap();
-    let structure = Structure::builder().pdb(ac).build();
+    let structure = Structure::builder()
+        .pdb(ac)
+        .material(chalky)
+        .rendertype(RenderOptions::BallAndStick)
+        .color_scheme(ColorScheme::ByAtomType)
+        .build();
+
     let mesh: Mesh = structure.to_mesh();
 
-    // mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    let rerun_mesh: Mesh3D = mesh.to_rerun().unwrap();
 
-    // Convert Bevy Mesh to Rerun Mesh3D
-    let rerun_mesh = mesh.to_rerun();
-
-    println!("Converted Bevy mesh to Rerun mesh3d: {:?}", rerun_mesh);
-
-    let (rec, storage) = rerun::RecordingStreamBuilder::new("rerun_example_dna_abacus").memory()?;
+    let (rec, storage) = rerun::RecordingStreamBuilder::new("rerun_protein_rendering").memory()?;
     let main_thread_token = rerun::MainThreadToken::i_promise_i_am_on_the_main_thread();
-    //re_log::setup_logging();
 
-    const NUM_POINTS: usize = 100;
+    // const NUM_POINTS: usize = 100;
+    // let (points1, colors1) = color_spiral(NUM_POINTS, 2.0, 0.02, 0.0, 0.1);
+    // let (points2, colors2) = color_spiral(NUM_POINTS, 2.0, 0.02, TAU * 0.5, 0.1);
 
-    let (points1, colors1) = color_spiral(NUM_POINTS, 2.0, 0.02, 0.0, 0.1);
-    let (points2, colors2) = color_spiral(NUM_POINTS, 2.0, 0.02, TAU * 0.5, 0.1);
+    // rec.log(
+    //     "dna/structure/left",
+    //     &rerun::Points3D::new(points1.iter().copied())
+    //         .with_colors(colors1)
+    //         .with_radii([0.08]),
+    // )?;
+    // rec.log(
+    //     "dna/structure/right",
+    //     &rerun::Points3D::new(points2.iter().copied())
+    //         .with_colors(colors2)
+    //         .with_radii([0.08]),
+    // )?;
 
-    rec.log(
-        "dna/structure/left",
-        &rerun::Points3D::new(points1.iter().copied())
-            .with_colors(colors1)
-            .with_radii([0.08]),
-    )?;
-    rec.log(
-        "dna/structure/right",
-        &rerun::Points3D::new(points2.iter().copied())
-            .with_colors(colors2)
-            .with_radii([0.08]),
-    )?;
+    rec.log("protein/structure/exmaple", &rerun_mesh)?;
 
     rerun::native_viewer::show(main_thread_token, storage.take())?;
 
