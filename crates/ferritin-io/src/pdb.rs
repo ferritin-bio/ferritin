0481d1ad98609204285830808f27929e51ce575d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-03-19 21:22:08 -0400

    update deps

--- crates/ferritin-io/src/pdb.rs
+++ crates/ferritin-io/src/pdb.rs
@@ -554,7 +554,6 @@ fn truncate_id(id: i32, max_id: i32) -> i32 {
 mod tests {
     use super::*;
     use ferritin_test_data::TestFile;
-    use std::path::Path;
 
     #[test]
     fn test_pdb_file_read() {
