5545f2a598396c0daf2019c4f8b0ea20fa99a6a5
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-04-10 11:24:01 -0400

    Add rotary which takes from falcon

---
+++ crates/ferritin-plms/src/esm2/models/rotary.rs
@@ -0,0 +1,72 @@
+use super::esm2::ESM2Config;
+use candle_core::{D, DType, Device, Result, Tensor};
+use candle_nn::{Embedding, LayerNorm, Linear, Module, VarBuilder, embedding, linear_b as linear};
+use serde::Deserialize;
+
+const MAX_SEQ_LEN: usize = 5000;
+
+fn rotate_half(x: &Tensor) -> Result<Tensor> {
+    let l = x.dim(D::Minus1)?;
+    let x1 = x.narrow(D::Minus1, 0, l / 2)?;
+    let x2 = x.narrow(D::Minus1, l / 2, l - l / 2)?;
+    let x21 = Tensor::cat(&[&x2.neg()?, &x1], D::Minus1)?;
+    Ok(x21)
+}
+
+#[derive(Debug, Clone)]
+struct FalconRotaryEmbedding {
+    inv_freq: Tensor,
+    cache: Option<(usize, Tensor, Tensor)>,
+}
+
+impl FalconRotaryEmbedding {
+    fn load(device: &Device, cfg: &ESM2Config) -> Result<Self> {
+        // let head_dim = cfg.head_dim();
+        let head_dim = cfg.head_dim();
+        let inv_freq: Vec<_> = (0..head_dim)
+            .step_by(2)
+            .map(|i| 1f32 / 10000f32.powf(i as f32 / head_dim as f32))
+            .collect();
+        Ok(Self {
+            inv_freq: Tensor::new(inv_freq.as_slice(), device)?,
+            cache: None,
+        })
+    }
+
+    fn cos_sin(
+        &mut self,
+        seq_len: usize,
+        device: &Device,
+        dtype: DType,
+    ) -> Result<(Tensor, Tensor)> {
+        match &self.cache {
+            Some((s, cos, sin)) if *s == seq_len => {
+                return Ok((cos.clone(), sin.clone()));
+            }
+            _ => {}
+        }
+        let t = Tensor::arange(0, seq_len as u32, device)?.to_dtype(dtype)?;
+        let inv_freq = self.inv_freq.to_dtype(dtype)?;
+        let freqs = t.unsqueeze(1)?.matmul(&inv_freq.unsqueeze(0)?)?;
+        let emb = Tensor::cat(&[&freqs, &freqs], D::Minus1)?;
+        let cos = emb.cos()?;
+        let sin = emb.sin()?;
+        self.cache = Some((seq_len, cos.clone(), sin.clone()));
+        Ok((cos, sin))
+    }
+
+    fn forward(
+        &mut self,
+        query: &Tensor,
+        key: &Tensor,
+        past_kv_len: usize,
+    ) -> Result<(Tensor, Tensor)> {
+        let (_batch, seq_len, _head_dim) = query.dims3()?;
+        let (cos, sin) = self.cos_sin(MAX_SEQ_LEN, query.device(), query.dtype())?;
+        let cos = cos.narrow(0, past_kv_len, seq_len)?;
+        let sin = sin.narrow(0, past_kv_len, seq_len)?;
+        let qs = (query.broadcast_mul(&cos)? + &rotate_half(query)?.broadcast_mul(&sin)?)?;
+        let ks = (key.broadcast_mul(&cos)? + &rotate_half(key)?.broadcast_mul(&sin)?)?;
+        Ok((qs, ks))
+    }
+}
