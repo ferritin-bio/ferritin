cc84b764956a8b55a40c9be11a5597dadb5add3c
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-06-11 12:23:55 -0400

    fix ORT for RC10

--- crates/ferritin-onnx-models/src/models/ligandmpnn/mod.rs
+++ crates/ferritin-onnx-models/src/models/ligandmpnn/mod.rs
@@ -23,8 +23,9 @@ use ort::{
         Session,
         builder::{GraphOptimizationLevel, SessionBuilder},
     },
+    value::Tensor as OrtTensor,
 };
-use std::{any::Any, path::PathBuf};
+use std::path::PathBuf;
 
 type NdArrayF32 = ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<ndarray::IxDynImpl>>;
 type NdArrayI64 = ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<ndarray::IxDynImpl>>;
@@ -67,6 +68,7 @@ impl LigandMPNN {
             decoder_path,
         })
     }
+
     fn create_session() -> Result<SessionBuilder> {
         ort::init()
             .with_name("LigandMPNN")
@@ -76,6 +78,7 @@ impl LigandMPNN {
             .with_optimization_level(GraphOptimizationLevel::Level1)?
             .with_intra_threads(1)?)
     }
+
     fn load_model_paths(model_type: ModelType) -> Result<(PathBuf, PathBuf)> {
         let api = Api::new()?;
         let (repo_id, encoder_name, decoder_name) = model_type.get_paths();
@@ -84,38 +87,44 @@ impl LigandMPNN {
             api.model(repo_id.to_string()).get(&decoder_name)?,
         ))
     }
+
     pub fn run_model(&self, ac: AtomCollection, position: i64, temperature: f32) -> Result<Tensor> {
         let (h_v, h_e, e_idx) = self.run_encoder(&ac)?;
         self.run_decoder(h_v, h_e, e_idx, temperature, position)
     }
+
     pub fn run_encoder(&self, ac: &AtomCollection) -> Result<(NdArrayF32, NdArrayF32, NdArrayI64)> {
         let device = device()?;
-        let encoder_model = self.session.clone().commit_from_file(&self.encoder_path)?;
+        let mut encoder_model = self.session.clone().commit_from_file(&self.encoder_path)?;
         let x_bb = ac.to_numeric_backbone_atoms(&device)?;
         let (lig_coords, lig_elements, lig_mask) = ac.to_numeric_ligand_atoms(&device)?;
+
         let coords_nd = tensor_to_ndarray_f32(x_bb)?;
         let lig_coords_nd = tensor_to_ndarray_f32(lig_coords)?;
         let lig_types_nd = tensor_to_ndarray_i64(lig_elements)?;
         let lig_mask_nd = tensor_to_ndarray_f32(lig_mask)?;
+
         let encoder_inputs = ort::inputs![
-            "coords" => coords_nd,
-            "ligand_coords" => lig_coords_nd,
-            "ligand_types" => lig_types_nd,
-            "ligand_mask" => lig_mask_nd
-        ]?;
+            "coords" => OrtTensor::from_array(coords_nd)?,
+            "ligand_coords" => OrtTensor::from_array(lig_coords_nd)?,
+            "ligand_types" => OrtTensor::from_array(lig_types_nd)?,
+            "ligand_mask" => OrtTensor::from_array(lig_mask_nd)?
+        ];
+
         let encoder_outputs = encoder_model.run(encoder_inputs)?;
         Ok((
             encoder_outputs["h_V"]
-                .try_extract_tensor::<f32>()?
+                .try_extract_array::<f32>()?
                 .to_owned(),
             encoder_outputs["h_E"]
-                .try_extract_tensor::<f32>()?
+                .try_extract_array::<f32>()?
                 .to_owned(),
             encoder_outputs["E_idx"]
-                .try_extract_tensor::<i64>()?
+                .try_extract_array::<i64>()?
                 .to_owned(),
         ))
     }
+
     pub fn run_decoder(
         &self,
         h_v: NdArrayF32,
@@ -124,27 +133,28 @@ impl LigandMPNN {
         temperature: f32,
         position: i64,
     ) -> Result<Tensor> {
-        let decoder_model = self.session.clone().commit_from_file(&self.decoder_path)?;
+        let mut decoder_model = self.session.clone().commit_from_file(&self.decoder_path)?;
+
         let position_tensor =
-            ort::value::Tensor::from_array(ndarray::Array::from_shape_vec([1], vec![position])?)?;
-        let temp_tensor = ort::value::Tensor::from_array(ndarray::Array::from_shape_vec(
-            [1],
-            vec![temperature],
-        )?)?;
+            OrtTensor::from_array(ndarray::Array::from_shape_vec([1], vec![position])?)?;
+        let temp_tensor =
+            OrtTensor::from_array(ndarray::Array::from_shape_vec([1], vec![temperature])?)?;
+
         let decoder_inputs = ort::inputs![
-            "h_v" => h_v,
-            "h_e" => h_e,
-            "e_idx" => e_idx,
+            "h_v" => OrtTensor::from_array(h_v)?,
+            "h_e" => OrtTensor::from_array(h_e)?,
+            "e_idx" => OrtTensor::from_array(e_idx)?,
             "position" => position_tensor,
-            "temperature" => temp_tensor,
-        ]?;
+            "temperature" => temp_tensor
+        ];
 
         let decoder_outputs = decoder_model.run(decoder_inputs)?;
         let logits = decoder_outputs["logits"]
-            .try_extract_tensor::<f32>()?
+            .try_extract_array::<f32>()? // Changed from try_extract_tensor
             .to_owned();
         ndarray_to_tensor_f32(logits)
     }
+
     pub fn get_single_location(
         &self,
         ac: AtomCollection,
@@ -164,6 +174,7 @@ impl LigandMPNN {
         }
         Ok(amino_acid_probs)
     }
+
     pub fn get_all_locations(&self, temp: f32) -> Result<Vec<PseudoProbability>> {
         todo!()
     }
@@ -174,6 +185,7 @@ mod tests {
     use super::*;
     use ferritin_core::load_structure;
     use ferritin_test_data::TestFile;
+
     fn setup_test_data() -> AtomCollection {
         let (protfile, _handle) = TestFile::protein_01().create_temp().unwrap();
         load_structure(protfile).unwrap()
